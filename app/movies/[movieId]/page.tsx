cdbef1faf9e265109ac83f3edb2e4a7795e30c53
Guillaume CAUCHOIS <guillobits@gmail.com>
2024-05-25 23:47:09 +0200

    [MoveDetails] Add

---
+++ app/movies/[movieId]/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+import { Movie, fetchMovie, fetchMovieRecommandations } from "@/lib/tmdb";
+import { Badge } from "@/components/ui/badge";
+import { MovieCard } from "@/components/movie-card";
+
+export default function MovieDetails({
+  params,
+}: {
+  params: { movieId: string };
+}) {
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [movieRecommentations, setMovieRecommentations] = useState<Movie[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const movieId = parseInt(params.movieId);
+    fetchMovie(movieId)
+      .then((movie) => {
+        setMovie(movie);
+        fetchMovieRecommandations(movieId).then((recommandations) =>
+          setMovieRecommentations(recommandations)
+        );
+      })
+      .catch(() => {
+        setError("Error while fetching the movie details");
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      {movie ? (
+        <div className="flex flex-col gap-5">
+          {/* Hero */}
+          <div className="relative min-h-[200px]">
+            {movie.backdrop_path ? (
+              <>
+                {/* Hero Image Background */}
+                <Image
+                  className="object-cover w-full md:h-[600px] h-screen"
+                  src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                  alt={movie.original_title}
+                  width={1920}
+                  height={800}
+                />
+                {/* Hero Overlay */}
+                <div className="bg-gradient-to-r from-slate-900/70 via-50% via-slate-900/70 to-80% w-full h-full absolute top-0 left-0 right-0 bottom-0" />
+              </>
+            ) : null}
+
+            {/* Hero Content */}
+            <div className="absolute top-0 bottom-0 left-0 w-full">
+              <div className="container h-full">
+                <div className="flex flex-col gap-5 lg:w-1/2 h-full justify-center text-white">
+                  <h1 className="text-4xl italic">{movie.original_title}</h1>
+                  <span>Released on {movie.release_date}</span>
+                  <p>{movie.overview}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <h2 className="text-3xl">Recommendations</h2>
+            {movieRecommentations.length > 0 ? (
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-4">
+                {movieRecommentations.map((movieRecommended) => (
+                  <MovieCard
+                    key={movieRecommended.id}
+                    movie={movieRecommended}
+                  />
+                ))}
+              </div>
+            ) : (
+              <p>No recommendation for this movie</p>
+            )}
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+}
